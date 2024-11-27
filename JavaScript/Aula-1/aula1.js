47ab4a4012ab28f872bdcdec57c3b1b7faaf1566
camomacedo259 <cam.omacedo@outlook.com>
2024-11-27 17:11:15 -0300

    first commit

---
+++ JavaScript/Aula-1/aula1.js
@@ -0,0 +1,44 @@
+
+//let: para dados que podem ser alterados
+//const: para dados que não podem ser alterados
+
+//Tipos de dados
+//String: texto
+//Number: número
+//Boolean: true ou false
+//Array: lista de dados
+//null: valor nulo
+//undefined: valor indefinido
+
+// Operãoes matemáticas
+
+let soma = 10 + 5;  //15
+let produto = 20 * 2; //40
+let divisao = 100 / 2; //50
+let subtracao = 50 - 20; //30
+
+
+// Estruturas de controle
+
+//if 
+if (soma > 20) {
+    conseole.log("A soma é maior que 20");
+}
+
+//if else - Executa um bloco de código um número determinado de vezes.
+else { 
+    console.log("A soma e menor ou igual a 20");
+}
+
+//loop for
+for (let i =0; i < 5; i++) {
+    conseole.log(i);
+    i++;   
+}
+
+//loop while - Executa um bloco de código enquanto uma condição for verdadeira.
+let i = 0;
+while (i < 5) {
+    conseole. log(i);
+    i++;
+}
---
+++ JavaScript/Aula-1/index.js
@@ -0,0 +1,17 @@
+// Definindo as variáveis
+
+
+let a = 10;
+let b = 5;
+
+// Realizando as operações e armazenando os resultados
+let resultadoSoma = a + b;
+let resultadoSubtracao = a - b;
+let resultadoMultiplicacao = a * b;
+let resultadoDivisao = a / b;
+
+// Exibindo os resultados
+console.log("O resultado da soma de a e b é:", resultadoSoma); // Saída: 15
+console.log("O resultado da subtração de a e b é:", resultadoSubtracao); // Saída: 5
+console.log("O resultado da multiplicação de a e b é:", resultadoMultiplicacao); // Saída: 50
+console.log("O resultado da divisão de a e b é:", resultadoDivisao); // Saída: 2
