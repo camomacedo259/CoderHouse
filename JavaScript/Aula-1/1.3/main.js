48559bbb036452383e5321a43008d4774da2e0a8
camomacedo259 <cam.omacedo@outlook.com>
2024-11-28 19:15:10 -0300

    first commit

---
+++ JavaScript/Aula-1/1.3/main.js
@@ -0,0 +1,20 @@
+
+// Variaveis  e uma forma de armazenar dados na memoria do computador
+
+//Declaração
+let clientes;
+
+//Atribuição
+clientes = "João";
+
+clientes = "Maria";
+
+console.log(clientes);
+
+//Inicialização
+let idade = 37;
+console.log(idade);
+
+//Constantes: É um tipo de variável que não pode ser alterada
+const anonas = 1987;
+console.log(anonas);
